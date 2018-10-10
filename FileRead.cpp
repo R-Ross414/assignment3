820abf352bb14843352069bb23a563db561322fe
R-Ross414 <rross@chapman.edu>
2018-10-10 23:20:19 +0000

    commit 2

---
+++ FileRead.cpp
@@ -0,0 +1,61 @@
+
+#include "FileRead.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+//////////////////////////////////////////////////////////////////////
+FileRead::FileRead()
+{
+  FileName = "";
+  lines = NULL;
+}
+
+//////////////////////////////////////////////////////////////////////
+FileRead::FileRead(string fileName)
+{
+  FileName = fileName;
+  Stream.open(fileName);
+  string line = "";
+  int i = 0;
+
+  while(getline(Stream, line))
+  {
+    i++;
+  }
+
+  line_no = i;
+  lines = new string[line_no];
+
+  Stream.close();
+  Stream.open(fileName);
+
+  i = 0;
+
+  while(getline(Stream, line))
+  {
+    lines[i] = line;
+    i++;
+  }
+}
+
+FileRead::~FileRead(){}
+
+//////////////////////////////////////////////////////////////
+void FileRead::printFile()
+{
+  string line = "";
+
+  for (int i = 0; i < line_no; i++)
+  {
+    cout << lines[i] << endl;
+  }
+
+  Stream.close();
+  Stream.open(FileName);
+}
+
+string FileRead::getLine(int linenum)
+{
+  return lines[linenum];
+}
--- main.cpp
+++ main.cpp
@@ -7,70 +7,111 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  //trying to make cmd line arg work
-  //string FileName;
-  cout << "Enter a source code file to analyze" << endl;
-  cout << "Analyzing 'Input.cpp'..." << endl;
-  //cin >> FileName;
-  //FileRead* file = new FileRead(argc);
-  FileRead* file = new FileRead("Input.cpp");
-  //file->printFile();
+  bool Yes;
+  Yes = true;
 
-  GenStack<string> codeLines();
+////////////////////////////////////////////////////////////////////
+while(Yes)
+{
+  string cmd = argv[1];
+  cout << "Enter a source code file to analyze" << endl;
+  cout << "Analyzing " << cmd << "..." << endl;
+  FileRead myfile(cmd);
+
+  //objects for the stack temp class
   GenStack<char> delim;
 
+////////////////////////////////////////////////////////////////////
   int i = 0;
-  while(i < file->line_no)
+  while (i < myfile.line_no)
   {
     int j = 0;
-    string line = file->getLine(i);
+    string line = myfile.getLine(i);
+
     while(j < line.length())
     {
+////////////////////////////////////////////////////////////////////
       if (line[j] == '(')
       {
         delim.push(line[j]);
       }
-
       else if (line[j] == '[')
       {
         delim.push(line[j]);
       }
-
+      else if (line[j] == '{')
+      {
+        delim.push(line[j]);
+      }
+////////////////////////////////////////////////////////////////////
       else if (line[j] == ')')
       {
         if (delim.isEmpty())
         {
-          cout << "Missing '(' at line " << i << endl;
+          cout << "Missing '(' at line " << endl;
           //turn into system exit /////////////
           break;
         }
-
         else if (delim.peek() == '(')
         {
           delim.pop();
         }
       }
-
+////////////////////////////////////////////////////////////////////
       else if (line[j] == ']')
       {
+        //cout << "hello world" << endl;
         if (delim.isEmpty())
         {
-          cout << "Missing '[' at line " << i << endl;
+          cout << "Missing '[' at line " << endl;
           //system.exit needed////////
           break;
         }
-
+        else if (delim.peek() != '[')
+        {
+          cout << "Missing '[' at line " << endl;
+          //system exit//////
+            break;
+        }
         else if (delim.peek() == '[')
         {
           delim.pop();
         }
       }
+////////////////////////////////////////////////////////////////////
+  else if (line[j] == '}')
+  {
+    if (delim.isEmpty())
+    {
+      cout << "Missing '{' at line " << i << endl;
+    }
+
+    else if (delim.peek() == '{')
+    {
+      cout << "Popping '{'" << endl;
+      delim.pop();
+    }
+  }
+
+////////////////////////////////////////////////////////////////////
       j++;
     }
 
+////////////////////////////////////////////////////////////////////
     if (!delim.isEmpty())
     {
-      cout << "Missing ')' at line " << i << endl;
+      if (delim.peek() == '(')
+      {
+        cout << "Missing ) at line " << i << endl;
+      }
+      else if (delim.peek() == '[')
+      {
+        cout << "Missing ] at line " << i << endl;
+      }
+      if (delim.peek() == '{')
+      {
+        cout << "Missing } at line " << i << endl;
+      }
       break;
     }
 
@@ -81,6 +122,25 @@ int main(int argc, char **argv)
       //store the index of the stack
     i++;
   }
+
+////////////////////////////////////////////////////////////////////
+char response;
+cout << "Would you like to enter another file location?" << endl;
+cout << "Enter 'Y' for yes, and 'N' for no." << endl;
+cin >> response;
+
+if(response == 'Y' || response == 'y')
+{
+  Yes = true;
+}
+if(response == 'N' || response == 'n')
+{
+  Yes = false;
+}
+
+}
+
+cout << "program done" << endl;
 
   /*GenStack<char> myStack(10);
 
@@ -102,7 +162,6 @@ int main(int argc, char **argv)
   {
     cout << "popping :" << myStack.pop() << endl;
   };
-
 */
   return 0;
 }
